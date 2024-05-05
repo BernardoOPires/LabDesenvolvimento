1bcc712c7642862daf9a8e11d70a05abfe669b27
Bernardo Pires <105243711+BernardoOPires@users.noreply.github.com>
2024-05-05 20:53:16 -0300

    Delete To-Do-List directory

--- To-Do-List/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS task;
-CREATE TABLE task (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  description VARCHAR(250) NOT NULL,
-  completed BOOLEAN,
-  startDate DATE,
-  dueDate DATE,
-  type VARCHAR(20)
-);
