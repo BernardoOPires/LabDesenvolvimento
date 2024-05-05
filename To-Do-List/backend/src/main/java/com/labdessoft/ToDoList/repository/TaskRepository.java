05b86c3e0fa439672befaebcfe7c268a704965e4
Bernardo Pires <105243711+BernardoOPires@users.noreply.github.com>
2024-05-05 20:57:33 -0300

    Delete To-Do-List/backend directory

--- To-Do-List/backend/src/main/java/com/labdessoft/ToDoList/repository/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.labdessoft.ToDoList.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.labdessoft.ToDoList.entity.Tasks;
-public interface TaskRepository extends JpaRepository<Tasks, Long> {
-    
-}
