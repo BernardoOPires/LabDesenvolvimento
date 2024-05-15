ea9f42565b78f143ad49c73e0a9aa41048f976de
Bernardo Pires <bernardoopires14@gmail.com>
2024-05-14 21:20:28 -0300

    correção geral

--- To-Do-List/backend/src/main/java/com/labdessoft/ToDoList/entity/Tasks.java
+++ To-Do-List/backend/src/main/java/com/labdessoft/ToDoList/entity/Tasks.java
@@ -1,15 +1,19 @@
-package com.labdessoft.roteiro01.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.Schema;  
 
 @Entity
 @Getter
